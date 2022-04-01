841bc10873b1b2247d6f1612e156fd453db506aa
bhanu-nunia <bhanu.nunia@gmail.com>
2022-04-01 20:36:08 +0530

    new file added

---
+++ getFlightNumber.js
@@ -0,0 +1,31 @@
+const puppeteer = require("puppeteer");
+
+async function getFlightNumber(url, idx) {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: "networkidle0" });
+  const btns = await page.$$('[data-testid="flight_card_bound_select_flight"]');
+  await btns[idx].click();
+  await page.waitForNetworkIdle();
+  const flnE = await page.waitForSelector(".css-h2u54n :nth-child(2)");
+  const flightNumber = await flnE.evaluate((el) => el.innerText.split(" ")[0]);
+  const clsBtn = await page.waitForSelector(
+    '[aria-label="flightdetails-modal"]'
+  );
+  await clsBtn.click();
+  await browser.close();
+
+  return flightNumber;
+}
+
+const url =
+  "https://flights.booking.com/flights/DEL-PNQ?adults=1&aid=304142&cabinClass=ECONOMY&children=&depart=2022-04-30&from=DEL&fromCountry=IN&fromLocationName=Delhi+International+Airport&label=gen173nr-1DCAEoggI46AdIM1gEaGyIAQGYAQm4ARfIAQzYAQPoAQGIAgGoAgO4AtLchpIGwAIB0gIkNjBiOThlMjYtM2ZmZS00NDQ2LTg4ZDgtMzIyYTk0ODRjOGI22AIE4AIB&sort=BEST&to=PNQ&toCountry=IN&toLocationName=Pune+International+Airport&type=ONEWAY";
+
+(async () => {
+  const flightNumbers = [];
+  for (let i = 0; i < 2; i++) {
+    const num = await getFlightNumber(url, i);
+    flightNumbers.push(num);
+  }
+  console.log(flightNumbers);
+})();
--- testing.js
+++ testing.js
@@ -1,3 +1,15 @@
+// (async () => {
+//     const browser = await puppeteer.launch({ headless: true })
+//     const page = await browser.newPage()
+//     await page.goto('https://github.com/login')
+//     await page.type('#login_field', process.env.GITHUB_USER)
+//     await page.type('#password', process.env.GITHUB_PWD)
+//     await page.click('[name="commit"]')
+//     await page.waitForNavigation()
+//     await page.screenshot({ path: screenshot })
+//     browser.close()
+//     console.log('See screenshot: ' + screenshot)
+//    })()
 
 const puppeteer = require('puppeteer');
 
@@ -7,12 +19,13 @@ const puppeteer = require('puppeteer');
 
     let url2 = "https://flights.booking.com/flights/DEL-PNQ/d7699_H4sIAAAAAAAA_y2Pa2-CMBiFf838RqG03Eyahc1LzATBZRr90kCpXOaoabtM_fWrYN435zznPf3SRuuLmtr26dzWjVZW24NaaFEXmgMmfuyTNFIK8d32tV200p7N11maIyfOtja0LTNsenjlV20pycikLTkoFCNZHD5ZEh_AY5wsc281nJjQBAPXDzLfW-8X3niUZJkvomRMFUlm6m9zP75t7iu0ucHPr07d0q7K026X7Gbbw_CMM0YgADiI4JBFoUgQPplpAp0RK022UYLSfIyaeE4UBGhIVwKhjyMAYYDdieJnznQr-g9-I_7cx8gyX71I17UOL-574fwyWRt4QbHZmjpGm0ELGgbGSooeJRutojhC3Hn0nOKwdFxDJ4rnxloKgQMeVUdXe2N32gS-6OU_qyTUSJMBAAA./?type=ONEWAY&adults=1&cabinClass=ECONOMY&children=&from=DEL&to=PNQ&fromCountry=IN&toCountry=IN&fromLocationName=Delhi+International+Airport&toLocationName=Pune+International+Airport&depart=2022-04-30&sort=BEST&aid=304142&label=gen173nr-1DCAEoggI46AdIM1gEaGyIAQGYAQm4ARfIAQzYAQPoAQGIAgGoAgO4AtLchpIGwAIB0gIkNjBiOThlMjYtM2ZmZS00NDQ2LTg4ZDgtMzIyYTk0ODRjOGI22AIE4AIB"
 
+    
     let browser = await puppeteer.launch();
-    let page = await browser.newPage();
+    let page2 = await browser.newPage();
 
 
-    await page.goto(url2, { waitUntil: 'networkidle0' });
-    let flightsData = await page.evaluate(() => {
+    await page2.goto(flightUrl, { waitUntil: 'networkidle0' });
+    let flightsData = await page2.evaluate(() => {
 
         let flights = document.querySelectorAll('.css-h2u54n .Text-module__root--variant-small_1___16UY4');
         // return flights.length;
@@ -21,7 +34,10 @@ const puppeteer = require('puppeteer');
         flights.forEach(flight => {
             let name = null, flightNumber = null, price = null, duration = null, iTime = null, fTime = null;
 
-            if (flight.querySelectorAll('.css-h2u54n .Text-module__root--variant-small_1___16UY4')[1]) {
+            if (flight.querySelectorAll('.Button-module__text___XTWR1')) {
+                page2.click('.Button-module__text___XTWR1')
+                page2.waitForNavigation()
+                if('.css-h2u54n .Text-module__root--variant-small_1___16UY4')
                 flightNumber = flight.querySelectorAll('.css-h2u54n .Text-module__root--variant-small_1___16UY4')[1].innerText;
             }
 
