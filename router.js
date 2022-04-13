fcdb663ef220114540feaf5416c8d6946555f19f
gopal1242 <gopalchbs2009@gmail.com>
2022-04-13 11:49:25 +0530

    changed by gopal

---
+++ router.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const { appendFile } = require("fs");
+const flightRouter=express.Router();
+// app.use('/',flightRouter);
+
+flightRouter.route('/')
+.get(getflightdata)
+// .post(postflight)
+
+
+
+
+
+
+module.exports=flightRouter;
+
+
